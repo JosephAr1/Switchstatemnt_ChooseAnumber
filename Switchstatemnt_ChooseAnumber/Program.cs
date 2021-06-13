42fc1d1b15d1feea77292f1fda79d1eb5aac0945
JosephAr1 <jonielhet@yahoo.com>
2021-06-13 20:26:09 +1200

    Add project files.

---
+++ Switchstatemnt_ChooseAnumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Switchstatemnt_ChooseAnumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number =4;
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("You Choose Number 1");
+                    break;
+                case 2:
+                    Console.WriteLine("You Choose Number 2");
+                    break;
+                case 3:
+                    Console.WriteLine("You Choose Number 3");
+                    break;
+                case 4:
+                    Console.WriteLine("You Choose Number 4");
+                    break;
+            }
+            
+        }
+    }
+}
+
